92b355b5ad686865e3124e869471d7a390e11ed9
SYSK4550 <102709429+SYSK4550@users.noreply.github.com>
2022-06-23 22:07:12 +0800

    Current Generated Web Application file

---
+++ Web Poultry/Models/Chicken.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Web_Poultry.Models
+{
+    public class Chicken
+    {
+        public int Id { get; set; }
+        public String ChickenType { get; set; }
+        public char ChickenGender { get; set; }
+        public int ChickenBirthWeight { get; set; }
+        public int ChickenBirthday { get; set; }
+        public String ProductType { get; set; }
+
+        public Chicken()
+        {
+
+        }
+    }
+}
